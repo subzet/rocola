b11d3a380c76b02b554912126b99ddb14fc6f24d
Manuel Ponsa <manuponsa@gmail.com>
2023-06-09 18:54:41 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -15,15 +15,58 @@ const token = process.env.SPOTIFY_TOKEN;
 
 const baseUrl = 'https://api.spotify.com/';
 
-const getSongsFromOpenAi = async () => {
-  const propmt = `Hey you're a great DJ. I need a playlist with 10 tracks to listen while working. Here are two songs I would like to have: "The Pretender" by "Foo Fighters" and "The Scientist" by "Coldplay". Please output just the song names in this style: <song_name>,<artist> separated by a new line and not output a single message other than the songs.`;
+const parseSongs = (songs: string) => {
+  const songsRaw = songs.split('\n');
 
-  const completion = await openai.createChatCompletion({
+  return songsRaw
+    .filter((song) => song[0] === '#')
+    .map((song) => song.replace('#', '').split(','));
+};
+
+const getSongsFromOpenAi = async (
+  prompt = "Hey I would like to listen to some music while working. I like to listen to rock and pop. I like the songs 'Smells Like Teen Spirit' by Nirvana, 'The Scientist' by Coldplay and 'I Want To Break Free' by Queen."
+) => {
+  const systemPrompt = `
+  You are a great DJ. 
+  I would prompt you with a playlist request with some requested song or styles I would like to hear. 
+  The playlist should have at least 10 songs unless the user requests more. 
+  The format of the output should be: #<song_name>,<artist> and each song should be in a new line. The starting '#' of each song is important to identify the song. 
+  Please just output the songs in the format requested and nothing else with no previous or after text.
+  Here is a sample output:
+  
+  #Smells Like Teen Spirit,Nirvana \n
+  #The Scientist,Coldplay \n
+  #I Want To Break Free,Queen \n
+  
+  `;
+
+  const songCompletion = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
-    messages: [{ role: 'user', content: propmt }],
+    messages: [
+      { role: 'system', content: systemPrompt },
+      { role: 'user', content: prompt },
+    ],
   });
 
-  return completion.data.choices[0].message?.content;
+  const playlistName = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'user',
+        content:
+          "Please create a short name for the playlist you've recently created",
+      },
+    ],
+  });
+
+  console.log(songCompletion.data.choices[0].message?.content);
+
+  return {
+    songs: parseSongs(
+      songCompletion.data.choices[0].message?.content as string
+    ),
+    playlistName: playlistName.data.choices[0].message?.content,
+  };
 };
 
 const search = async (song: string, artist: string) => {
@@ -84,15 +127,17 @@ const addTracks = async (playlistId: string, tracks: string[]) => {
 /* eslint-disable unicorn/no-process-exit */
 
 async function main() {
-  const songs = await getSongsFromOpenAi();
-
-  const songsArray = songs?.split('\n').map((song) => song.split(','));
-
-  const tracksUri = await Promise.all(
-    songsArray?.map((song) =>
-      search(song[0], song[1]).then((result) => result.tracks.items[0].uri)
-    ) ?? []
+  const { songs, playlistName } = await getSongsFromOpenAi(
+    'Hey I would like to hear songs that helps me focus while working, create a playlist with 10 songs'
   );
+
+  const tracksUri: string[] =
+    songs?.map(
+      async (song) =>
+        await search(song[0], song[1]).then(
+          (result) => result.tracks.items[0].uri
+        )
+    ) ?? [];
 
   if (!tracksUri || tracksUri.length === 0) {
     throw new Error('No tracks found');
@@ -100,7 +145,10 @@ async function main() {
 
   const user = await getUser();
 
-  const playlist = await createPlaylist(user.id, 'My playlist API');
+  const playlist = await createPlaylist(
+    user.id,
+    playlistName ?? 'My API playlist'
+  );
 
   await addTracks(playlist.id, tracksUri);
 }
